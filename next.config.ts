6fab44d4144c875f6541042567ac42daeb1ed259
Brandon-Wong801 <wong.brandon808@gmail.com>
2025-07-22 20:14:07 -0700

    update basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/personal-site" : "", 
+  basePath: "/personal-site",
   images: {
     unoptimized: true, 
   },
