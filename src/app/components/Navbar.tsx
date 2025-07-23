6ac899484afe40d6378665327045c4e7b4183a0e
Brandon-Wong801 <wong.brandon808@gmail.com>
2025-07-22 21:24:03 -0700

    revert basepath on Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,12 +10,12 @@ const Navbar = () => {
             <span className="absolute inset-0 bg-gray-700 rounded transition-all duration-300 scale-0 group-hover:scale-100"></span>
             <span className="relative z-10 hover:text-gray-300 transition duration-300">about</span>
           </Link>
-          <Link href={`${basePath}/pictures`} className="relative px-3 py-2 group">
+          <Link href="/pictures" className="relative px-3 py-2 group">
             <span className="absolute inset-0 bg-gray-700 rounded transition-all duration-300 scale-0 group-hover:scale-100"></span>
             <span className="relative z-10 hover:text-gray-300 transition duration-300">pictures</span>
           </Link>
           <Link
-            href={`${basePath}/resume`}
+            href="/resume"
             className="relative px-3 py-2 group"
           >
             <span className="absolute inset-0 bg-gray-700 rounded transition-all duration-300 scale-0 group-hover:scale-100"></span>
