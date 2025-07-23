f3fa0af63fecdcf2b22f623881b348173643ddd9
Brandon-Wong801 <wong.brandon808@gmail.com>
2025-07-22 20:42:14 -0700

    update to link in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,15 +14,13 @@ const Navbar = () => {
             <span className="absolute inset-0 bg-gray-700 rounded transition-all duration-300 scale-0 group-hover:scale-100"></span>
             <span className="relative z-10 hover:text-gray-300 transition duration-300">pictures</span>
           </Link>
-          <a
+          <Link
             href={`${basePath}/resume`}
-            target="_blank"
-            rel="noopener noreferrer"
             className="relative px-3 py-2 group"
           >
             <span className="absolute inset-0 bg-gray-700 rounded transition-all duration-300 scale-0 group-hover:scale-100"></span>
             <span className="relative z-10 hover:text-gray-300 transition duration-300">resume</span>
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
